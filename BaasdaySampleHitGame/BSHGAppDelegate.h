b5d3f33608c8879d20ad00bf75152e7812adba4c
Yuu Shimizu <yuu@nyampass.com>
2013-04-25 09:40:54 +0900

    Initial Commit

---
+++ BaasdaySampleHitGame/BSHGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BSHGAppDelegate.h
+//  BaasdaySampleHitGame
+//
+//  Created by Yuu Shimizu on 4/25/13.
+//  Copyright (c) 2013 Nyampass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSHGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BaasdaySampleHitGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BaasdaySampleHitGame
+//
+//  Created by Yuu Shimizu on 4/25/13.
+//  Copyright (c) 2013 Nyampass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BSHGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([BSHGAppDelegate class]));
+	}
+}
